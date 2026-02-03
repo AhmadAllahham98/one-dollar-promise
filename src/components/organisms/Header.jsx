7f9d2c9531e41b23dbc7480e927dd28d38b89e3f
Ahmad Allahham <ahmad.allahham.1998@gmail.com>
2026-02-03 14:01:53 +0800

    Animated the background + added padding to the logo in the header

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -6,13 +6,13 @@ import { Button } from "../atoms/Button";
 export const Header = ({ user = null, className = "", onLogout, ...props }) => {
   return (
     <header
-      className={`flex flex-row items-center justify-between w-full bg-surface-100 p-md h-[64px] md:p-lg md:h-[84px] ${className}`}
+      className={`flex flex-row items-center justify-between w-full p-md h-[64px] md:p-lg md:h-[84px] ${className}`}
       {...props}
     >
       <img
         src={logo}
         alt="One Dollar Promise Logo"
-        className="h-full w-auto object-contain shrink-0"
+        className="p-xsm h-full w-auto object-contain shrink-0"
       />
       <div className="flex items-center gap-md ml-auto">
         {user && (
